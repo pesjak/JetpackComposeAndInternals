f84a575ca020c697c9f66e2f27530df22de734e0
Primož Pesjak <primoz.pesjak@gmail.com>
2024-09-20 10:26:47 +0200

    Exercise 2

--- app/src/main/java/dev/jorgecastillo/compose/app/ui/composables/SocialNetworkUser.kt
+++ app/src/main/java/dev/jorgecastillo/compose/app/ui/composables/SocialNetworkUser.kt
@@ -3,7 +3,9 @@ package dev.jorgecastillo.compose.app.ui.composables
 import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -14,7 +16,16 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun SocialNetworkUser(name: String, location: String, onFollow: () -> Unit) {
-    
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column {
+            Text(name)
+            Text(location)
+        }
+        Spacer(Modifier.width(16.dp))
+        Button(onFollow) { Text("Follow") }
+    }
 }
 
 @Composable
